df42bee4077182bbd62f36bacd75ec703ae81193
ebnu <78129927+ebnu@users.noreply.github.com>
2021-05-30 16:19:55 +0700

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>Todo-Frontend</h1>
+    <ul>
+      <li v-for="item in todos" :key="item.id">{{item.deskripsi}} <button @click="hapus(item.id)">x</button></li>
+    </ul>
+    <input v-model="myText"/>
+    <button @click="tambah">Add</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data: function(){
+    return {
+      todos: [],
+      myText: ''
+    }
+  },
+  created: function(){
+    axios.get('http://localhost:3000/todo')
+    .then((response) => {
+      this.todos = response.data
+    })
+  },
+  methods:{
+    tambah: function(){
+      const newItem = {deskripsi: this.myText}
+      axios.post('http://localhost:3000/todo', newItem)
+      .then(() => {
+        this.todos.push(newItem)
+        window.location.reload()
+      })
+    },
+    hapus: function(id){
+      var index = this.todos.findIndex(obj => obj.id === id)
+      axios.delete(`http://localhost:3000/todo/${id}`)
+      .then(() => {
+        this.todos.splice(index, 1)
+      })
+    }
+  }
+}
+</script>
